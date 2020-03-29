26494ae4e7cd3dc9f04c0d36e41acf1378198223
Ritik Thool <ritikthool2000@gmail.com>
2020-03-30 02:29:53 +0530

    yeah

--- Geca/settings.py
+++ Geca/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ut1ytdx49faru6t$e0y!6j97xxgbulyz_%ec=p9epr*-n5i9l%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
