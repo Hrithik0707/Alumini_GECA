56b792d6eb7cf0f3647d19eb9748b5d2868c45cc
Ritik Thool <ritikthool2000@gmail.com>
2020-03-30 23:06:37 +0530

    database issue solved

--- Alumini/models.py
+++ Alumini/models.py
@@ -11,7 +11,7 @@ class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add = True)
-    author = models.ForeignKey(User,default=1, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
