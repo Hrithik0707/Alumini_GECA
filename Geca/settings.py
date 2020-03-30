4e1360dac22d749ee1a63fdf71e32f01252f61ec
Ritik Thool <ritikthool2000@gmail.com>
2020-03-30 11:05:00 +0530

    goo

--- Geca/settings.py
+++ Geca/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles','root')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
